86cb0186a778cb44201987e0fe623b1b7bf99ca8
TTatchell <tim.tatchell24@gmail.com>
2021-07-03 18:27:53 +1000

    upload

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,36 @@
-# code here!
+class School
+    attr_accessor :roster
+
+    def initialize(school)
+        @school = school
+        @roster = {}
+    end
+
+    def add_student(name, grade)
+        if @roster[grade]
+            @roster[grade] << name
+        elsif
+            @roster[grade] = []
+            @roster[grade] << name
+        end
+    end
+
+    def grade(grade)
+        @roster[grade]
+    end
+
+    def sort
+        @roster.sort_by(&:first).map { |k, v| [k, v.sort]}.to_h
+    end
+end
+
+school = School.new("Bayside High School")
+
+school.add_student("Homer Simpson", 9)
+school.add_student("Bart Simpson", 9)
+school.add_student("Avi Flombaum", 10)
+school.add_student("Jeff Baird", 10)
+school.add_student("Blake Johnson", 7)
+school.add_student("Jack Bauer", 7)
+
+puts school.sort
